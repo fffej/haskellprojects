baf02daabdbdbbb11f08ce9ce7c9855cfebb9380
Jeff Foster <jfoster@jfoster-desktop-64.(none)>
2009-11-25 14:14:14 +0000

    Minor changes at end of lunch break!

--- cards/Klondike.hs
+++ cards/Klondike.hs
@@ -33,6 +33,11 @@ data Game = Game
     , tableau :: Tableau
     } deriving (Show)
 
+-- |Create a new game from the given deck of cards (assumed a full deck)
+newGame :: [Card] -> Game
+newGame cards = Game deck emptyFoundation tableu where
+    (tableu,deck) = dealTableau cards
+
 -- |Create empty foundation
 emptyFoundation = Foundation (Base Spades []) (Base Clubs []) (Base Diamonds []) (Base Hearts [])
 
@@ -51,8 +56,6 @@ dealTableau deck = (Tableau
     (f,m) = splitAt 6 l
     (g,rest) = splitAt 7 m
 
-
-
 -- |Can the card move down from the deck to the given slot?
 cardDown :: Card -> Slot -> Bool
 cardDown (Card King _) (Slot [] _) = True
@@ -67,60 +70,15 @@ cardUpFromDeck (Card v s) (Base t (Card x b:xs)) = succ x == v && s == t
 
 -- |Can we move up from the particular slot to a base object?
 cardUpFromSlot :: Slot -> Base -> Bool
-cardUpFromSlot (Slot [] _) _ = False
 cardUpFromSlot (Slot (x:xs) _) b = cardUpFromDeck x b
 
 {-
-emptyFoundation = Foundation [] [] [] []
-
--- The various moves possible
-data Move = GameOver
-          | TurnDeck
-          | MoveFromDeck Slot
-          | MoveUp Slot -- the slot
-          | MoveUpFromDeck -- the suit is known from the card on the deck
-            deriving (Show)
-
-newGame :: [Card] -> Game
-newGame cards = Game deck emptyFoundation tableu where
-    (tableu,deck) = dealTableau cards
-
--- Must be a more compact way to do this (side effect clumsy)
-dealTableau :: [Card] -> (Tableau,[Card])
-dealTableau deck = (Tableau a b c d e f g,rest) where
-    (a,h) = splitAt 1 deck
-    (b,i) = splitAt 2 h
-    (c,j) = splitAt 3 i
-    (d,k) = splitAt 4 j
-    (e,l) = splitAt 5 k
-    (f,m) = splitAt 6 l
-    (g,rest) = splitAt 7 m
-
--- Given a foundation card (maybe empty) and a potential card to move up can we?
-cardUp :: (Maybe Card) -> Card -> Bool
-cardUp Nothing (Card Ace _) = True
-cardUp Nothing (Card value _) = False
-cardUp (Just x) (Card Ace _) = False
-cardUp (Just (Card fvalue fsuit)) (Card value suit) = fsuit == suit && fvalue == pred value
-
--- Given a card and a potential target on the tableau (might be empty), can we move it down?
-cardDown :: Card -> (Maybe Card) -> Bool
-cardDown (Card King _) Nothing = True
-cardDown _ Nothing = False
-cardDown a@(Card x _) (Just b@(Card y _)) = alternateColors a b && succ x == y
 
 -- TODO This should return the target card NOT the source card.  Should be list [Maybe Card]
 -- as a return type
 -- Where can we move the given card on the tableau?
 cardDownTableau :: Card -> Tableau -> [Card]
 cardDownTableau c = catMaybes . filter (cardDown c) . tableauCards
-
--- TODO Clearly this is another lump of gibberish.  What's the right way?
-getSuit :: Foundation -> Card -> [Card]
-getSuit f (Card _ Clubs) = clubs f;
-getSuit f (Card _ Diamonds) = diamonds f;
-getSuit f (Card _ Hearts) = hearts f;
-getSuit f (Card _ Spades) = spades f;
 
 moveUpTableau :: Foundation -> Card -> Maybe Card
 moveUpTableau f c@(Card Ace _) | null (getSuit f c) = Just c
