506ee17378e6ea2471922cefee382e2e8eb0b627
Jeff Foster <jfoster@jfoster-desktop-64.(none)>
2009-11-27 13:21:02 +0000

    Some simplifications and complications.  The code for updating structures is painfully crap.  TODO find a better way to do this.

--- cards/Klondike.hs
+++ cards/Klondike.hs
@@ -22,7 +22,7 @@ data Move = TurnDeck
             deriving (Show,Eq)
 
 data Base = Base Suit [Card] deriving (Eq,Show)
-data Foundation = Foundation Base Base Base Base 
+data Foundation = Foundation Base Base Base Base
                   deriving (Show)
 
 -- |A game consists of a single deck of cards, a foundation and a tableau
@@ -70,43 +70,39 @@ cardUpFromDeck (Card v s) (Base t (Card x b:xs)) = succ x == v && s == t
 
 -- |Can we move up from the particular slot to a base object?
 cardUpFromSlot :: Slot -> Base -> Bool
-cardUpFromSlot (Slot (x:xs) _) b = cardUpFromDeck x b
+cardUpFromSlot (Slot (x:_) _) = cardUpFromDeck x
 
--- | 
+-- |Make the moves
+move :: Game -> Move -> Game
+move g TurnDeck = Game (turnDeck (deck g)) (foundation g) (tableau g) where 
+    turnDeck [] = []
+    turnDeck (x:xs) = xs ++ [x]
 
-{-
+move g (ToFoundation (Slot (x:xs) id)) = Game d (addCard x f) t where
+    d = deck g
+    f = foundation g
+    t = updateTableau (Slot xs id) (tableau g)
 
--- TODO This should return the target card NOT the source card.  Should be list [Maybe Card]
--- as a return type
--- Where can we move the given card on the tableau?
-cardDownTableau :: Card -> Tableau -> [Card]
-cardDownTableau c = catMaybes . filter (cardDown c) . tableauCards
+move g (DeckTo (Slot xs id)) = Game rest (foundation g) t where
+    (c@(Card _ _):rest) = deck g
+    t = updateTableau (Slot (c:xs) id) (tableau g) 
+        
+move g (MoveCards s i t) = undefined
 
-moveUpTableau :: Foundation -> Card -> Maybe Card
-moveUpTableau f c@(Card Ace _) | null (getSuit f c) = Just c
-                               | otherwise = Nothing
-moveUpTableau f c@(Card v _) | null (getSuit f c) = Nothing
-                             | value (head (getSuit f c)) == pred v = Just c
+move g (MoveCard (Slot (s:ss) from) (Slot x to)) = Game (deck g) (foundation g) t where
+    u = updateTableau (Slot ss from) (tableau g)
+    t = updateTableau (Slot (s:x) to) u
 
-turnDeck :: [Card] -> [Card]
-turnDeck [] = []
-turnDeck (x:xs) = xs ++ [x]
+-- |Given an updated slot, update the create a new tableau reflecting this
+updateTableau :: Slot -> Tableau -> Tableau
+updateTableau = undefined
 
--- A list of available moves that can be played from the current position
-
-getMoves :: Game -> [Move]
-getMoves game | canTurnDeck = TurnDeck : cardDownMoves ++ cardUpMoves
-              | otherwise = cardDownMoves ++ cardUpMoves
-              where 
-                t = tableau game
-                f = foundation game
-                ts = (catMaybes . tableauCards) t
-                canTurnDeck = (not . null . deck) game
-                cardDownMoves = map MoveFromDeck (cardDownTableau ((head . deck) game) t)
-                cardUpMoves = map MoveUp (catMaybes (map (moveUpTableau f) ts))
-
-makeMove :: Game -> Move -> Game
-makeMove g TurnDeck = Game (turnDeck (deck g)) (foundation g) (tableau g)
-makeMove g (MoveFromDeck c) = undefined
-makeMove g (MoveUp c) = undefined
--}          
+-- |In the competition for uglist function that has ever existed there can be only one
+-- And here it is...  (TODO make look less moronic)
+addCard :: Card -> Foundation -> Foundation
+addCard t@(Card _ s) 
+        (Foundation w@(Base a as) x@(Base b bs) y@(Base c cs) z@(Base d ds)) 
+            | s == a = Foundation (Base a (t:as)) x y z
+            | s == b = Foundation w (Base b (t:bs)) y z
+            | s == c = Foundation w x (Base c (t:cs)) z
+            | s == d = Foundation w x y (Base d (t:ds))
