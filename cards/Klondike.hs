fa7b2a10a904620dde18f34012ca73866bae7128
Jeff Foster <jeff.foster@acm.org>
2009-12-12 20:47:30 +0000

    Continue refactoring the code until it doesn't look as embarassing.
    Remove particular embarassing function "heads" - what was I doing writing that in the first place?

--- cards/Klondike.hs
+++ cards/Klondike.hs
@@ -4,12 +4,10 @@ import Cards
 
 import Data.List
 import Data.Array
-import Data.Maybe
 import Ix
 
 -- |A type to index into the array
-data Index = A | B | C | D | E | F | G 
-           deriving (Eq, Ord, Show, Enum, Ix)
+data Index = A | B | C | D | E | F | G  deriving (Eq, Ord, Show, Enum, Ix)
 
 -- |A Slot is a pile of cards in the tableau, consisting of some shown cards and some hidden
 data Slot = Slot 
@@ -19,7 +17,8 @@ data Slot = Slot
     } deriving (Show,Eq)
 
 dropCard :: Slot -> Slot
-dropCard (Slot s h) = Slot ((take 1 h) ++ (drop 1 s)) (drop 1 h)
+dropCard (Slot s h) = Slot (f ++ drop 1 s) r where
+    (f,r) = splitAt 1 h
 
 -- |The tableu is the seven possible piles of cards
 type Tableau = Array Index Slot
@@ -54,7 +53,7 @@ empty (Slot s h) = null s && null h
 
 -- |Create empty foundation
 emptyFoundation :: Foundation
-emptyFoundation = array (Clubs,Spades) [(Clubs,[]),(Diamonds,[]),(Hearts,[]),(Spades,[])]
+emptyFoundation = array (Clubs,Spades) [(s,[]) | s <- [Clubs .. Spades]]
 
 -- |Deal the tableau from a selection of cards.  Assumes that length [Card] 
 -- is enough for this to succeed without error
@@ -81,40 +80,40 @@ successor a b = value a /= King && alternateColors a b && follows a b
 
 -- |Can the card move down from the deck to the given slot?
 cardDown :: Card -> Slot -> Bool
-cardDown card (Slot shown _) | null shown = value card == King
-                             | otherwise = successor card (head shown)
+cardDown card (Slot s _) | null s = value card == King
+                         | otherwise = successor card (head s)
 
 -- |Can the card move to foundation?
-cardUpFromDeck :: Card -> Foundation -> Bool
-cardUpFromDeck (Card v s) f | null cards = v == Ace
-                            | otherwise = x /=King && succ x == v 
+cardUp :: Card -> Foundation -> Bool
+cardUp (Card v suit) f | null cards = v == Ace
+                       | otherwise = x /=King && succ x == v 
                             where
-                              cards = f ! s
+                              cards = f ! suit
                               (Card x _) = head cards
 
 
 -- |Can we move up from the particular slot to a base object?
 cardUpFromSlot :: Slot -> Foundation -> Bool
 cardUpFromSlot (Slot x _) | null x = const False
-                          | otherwise = cardUpFromDeck (head x)
+                          | otherwise = cardUp (head x)
 
 dropCards :: Slot -> Int -> ([Card],Slot)
 dropCards (Slot from []) n = (cards,(Slot r [])) where
     (cards,r) = splitAt n from
-dropCards (Slot from (h:hs)) n = (cards,(Slot visible hidden)) where    
+dropCards (Slot from (h:hs)) n = (cards,(Slot visible hdn)) where    
     (cards,x) = splitAt n from
     visible = if null x then [h] else x
-    hidden = if null x then hs else h:hs
+    hdn = if null x then hs else h:hs
 
 -- |Can the card move from x to y?
--- TODO eliminate special cases
 slotMove :: Slot -> Slot -> Bool
-slotMove (Slot [] []) _ = False
-slotMove (Slot (x:_) _) s = cardDown x s
+slotMove (Slot from _) s | null from = False
+                         | otherwise = cardDown (head from) s
 
 -- |Turn the deck over
+turnOverDeck :: [Card] -> [Card]
 turnOverDeck [] = []
-turnOverDeck (x:xs) = xs
+turnOverDeck (_:xs) = xs
 
 move :: Game -> Move -> Game
 
@@ -143,9 +142,9 @@ move g (DeckTo i) = Game rest (foundation g) t where
 -- |Move the given number of cards between two slots
 move g (MoveCards fromIndex n toIndex) = Game (deck g) (foundation g) t where
     from = tableau g ! fromIndex
-    (Slot tos hidden) = tableau g ! toIndex
+    (Slot tos hdn) = tableau g ! toIndex
     (newMove,updatedFrom) = dropCards from n
-    updatedTo = Slot (newMove ++ tos) hidden
+    updatedTo = Slot (newMove ++ tos) hdn
     t = tableau g // [(fromIndex,updatedFrom),(toIndex,updatedTo)]
 
 -- |Given the stack of cards, find the longest sequence of cards
@@ -155,13 +154,7 @@ consecutiveCards (x:[]) = [x]
 consecutiveCards (x:y:xs) | successor x y = x : consecutiveCards (y:xs)
                           | otherwise = [x]
 
--- |Given a series of consecutive cards, produce all the initial occurrences
--- TODO less bizarre implementation!
-heads :: [a] -> [[a]]
-heads = init . map reverse . (tails . reverse)
-
--- |In the competition for uglist function that has ever existed there can be only one winner
--- And here it is...  (TODO make look less moronic)
+-- |Add the given card tot he foundation
 addCard :: Card -> Foundation -> Foundation
 addCard t@(Card _ s) f = f // [(s,(t:f!s))]
 
@@ -169,43 +162,37 @@ addCard t@(Card _ s) f = f // [(s,(t:f!s))]
 gameWon :: Game -> Bool
 gameWon game = all empty (elems (tableau game)) && not (null (deck game))
 
--- TODO MoveCards
+-- TODO Move more than 1 card at a time
 getMoves :: Game -> [Move]
-getMoves g  = movesFromDeckToFoundation dk 
-              ++ deckToSlot dk
-              ++ turnDeckMove 
+getMoves g  = [DeckUp | (not.null) dk && cardUp (head dk) (foundation g)]
+              ++ [(DeckTo . fst) x | not.null $ dk, x <- filter (cardDown (head dk) . snd) slots]
+              ++ [TurnDeck | not.null $ dk] 
               ++ [ToFoundation is | is <- cardsUp]
               ++ slotMoves 
               ++ [GameOver | gameWon g] where
     dk = deck g
     slots = assocs (tableau g)
-    f = assocs (foundation g)
-    turnDeckMove = [TurnDeck | not.null $ dk]
-    movesFromDeckToFoundation [] = []
-    movesFromDeckToFoundation (x:_) = [DeckUp | cardUpFromDeck x (foundation g)]
-    cardsUp = map fst (filter (((flip cardUpFromSlot) (foundation g)) . snd) slots)
-    deckToSlot [] = []
-    deckToSlot (z:ds) = map (DeckTo . fst) (filter (cardDown z . snd) slots)
+    cardsUp = map fst (filter (flip cardUpFromSlot (foundation g) . snd) slots)
     slotMoves = [MoveCards (fst x) 1 (fst y) | x <- slots, y <- slots, 
                                                fst x /= fst y && slotMove (snd x) (snd y)]
 
 -- |Play a game from the given state using the provider player function.  Get the list
--- of moves from the oringal state
+-- of moves from the original state
 playGame :: Game -> (Game -> [Move] -> Move) -> [Move]
-playGame g player = nextMove : playGame nextGame player where
+playGame g p = nextMove : playGame nextGame p where
     moves = getMoves g
-    nextMove = player g moves
+    nextMove = p g moves
     nextGame = move g nextMove
 
 replayMoves :: Game -> [Move] -> Game
 replayMoves = foldl move
 
 player :: Game -> [Move] -> Move
-player g [] = GameOver
-player g m = head $ sortBy compareMoves m 
+player _ [] = GameOver
+player _ m = head $ sortBy compareMoves m 
                                              
 compareMoves :: Move -> Move -> Ordering
-compareMoves a TurnDeck = LT
+compareMoves _ TurnDeck = LT
 compareMoves _ DeckUp = GT
 compareMoves _ (ToFoundation _) = GT
 compareMoves _ (MoveCards _ _ _) = LT
