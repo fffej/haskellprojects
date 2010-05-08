21d11a9fb4ec23d81bcecc236a79de89ff315d1b
Jeff Foster <jfoster@acm.org>
2010-05-08 22:13:47 +0100

    thin out code.

--- codejam/snapper/Snapper.hs
+++ codejam/snapper/Snapper.hs
@@ -25,54 +25,28 @@ powerInt x | x ==1 || x == 3 = True
          | otherwise = False
 
 
-flipState (Snapper state power) | power = Snapper (not state) power
-                                | otherwise = Snapper state power
-
 flipStateInt x | (powerInt x) = statePower (not (stateInt x)) (powerInt x)
                | otherwise = x
 
-turnOff :: Snapper -> Snapper
-turnOff (Snapper s p) = Snapper s False
-
 turnOffInt :: Int -> Int
 turnOffInt x = statePower (stateInt x) False
-
-updatePower :: [Snapper] -> Bool -> [Snapper]
-updatePower [] _ = []
-updatePower (Snapper state power:xs) previous | previous = Snapper state True : updatePower xs state
-                                              | otherwise = Snapper state False : map turnOff xs
 
 updatePowerInt :: [Int] -> Bool -> [Int]
 updatePowerInt [] _ = []
 updatePowerInt (x:xs) previous | previous = statePower (stateInt x) True : updatePowerInt xs (stateInt x)
                                | otherwise = statePower (stateInt x) False : map turnOffInt xs
                                                 
-click :: [Snapper] -> [Snapper]
-click xs = updatePower (map flipState xs) True
-
 clickInt :: [Int] -> [Int]
 clickInt xs = updatePowerInt (map flipStateInt xs) True
 
-snappers :: Int -> [Snapper]
-snappers n = Snapper False True : replicate (n-1) (Snapper False False)
-
 snappersInt :: Int -> [Int]
 snappersInt n = statePower False True : replicate (n-1) (statePower False False)
-
-lightBulb :: [Snapper] -> String
-lightBulb xs | power x && state x = "ON"
-             | otherwise = "OFF"
-    where
-      x = last xs
 
 lightBulbInt :: [Int] -> String
 lightBulbInt xs | powerInt x && stateInt x = "ON"
                 | otherwise = "OFF"
     where
       x = last xs
-
-runTest :: Int -> Int -> String
-runTest n k = lightBulb $ last $ take (k+1) $ iterate click (snappers n)
 
 runTestInt :: Int -> Int -> String
 runTestInt n k = lightBulbInt $ last $ take (k+1) $ iterate clickInt (snappersInt n)
