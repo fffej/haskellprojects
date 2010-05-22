78f4c83b5b2e08b97808221ec9092a7ccd78965d
Jeff Foster <jfoster@acm.org>
2010-05-22 10:15:18 +0100

    stemming algorithm that I've not finished.

--- stemming/Stemming.hs
+++ stemming/Stemming.hs
@@ -1,80 +1,41 @@
-module Stemming (main,parseRules) where
+module Stemming (parseRules) where
 
 import qualified Data.Map as Map
 import Data.Char (isLower)
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf,find,(!!))
 import System (getArgs)
-import System.Console.GetOpt
-
-
+import Data.Maybe (fromJust)
+import Test.HUnit
 import Text.ParserCombinators.Parsec
 
-data RuleType = Protect
-              | Intact
+data RuleType = Stop
               | Continue
-              | ContInt
-              | ProtInt
-              | Stop 
-                deriving (Show)
-        
+                deriving (Show,Eq)
+
 data Rule = Rule {
-      keyString :: String
-    , replaceString :: String
-    , ruleType :: RuleType
-} deriving (Show)
+      suffix :: String
+    , drop :: Int
+    , intact :: Bool
+    , action :: RuleType
+} deriving (Show,Eq)
 
-data RuleResult = RuleStop | RuleContinue | RuleNotApplied deriving (Eq)
-
-type Rules = Map.Map Char [Rule]
-
-makeRulesTable :: [Rule] -> Rules
-makeRulesTable = foldl insertRule Map.empty where
-    insertRule mp r@(Rule k _ _) = Map.insertWith (++) (last k) [r] mp
-
-rules :: GenParser Char st Rules
-rules = do
-  result <- many rule
+parseRules :: GenParser Char st [Rule]
+parseRules = do
+  result <- many parseRule
   eof
-  return (makeRulesTable result)
+  return result
 
-rule :: GenParser Char st Rule
-rule = do
-  keyString <- many (noneOf ",")
-  char ','
-  replaceString <- many (noneOf (","))
-  char ','
-  ruleType <- parseRuleType
-  many (noneOf "\n")
-  return (Rule keyString replaceString ruleType)
-
--- |http://legacy.cs.uu.nl/daan/download/parsec/parsec.html 
--- See the section of consuming input for the reasoning on try
-parseRuleType :: GenParser Char st RuleType
-parseRuleType = do
-  (try $ string "intact" >> return Intact)
-  <|> (try $ string "continue" >> return Continue)
-  <|> (try $ string "contint" >> return ContInt)
-  <|> (try $ string "protect" >> return Protect)
-  <|> (try $ string "protint" >> return ProtInt)
-  <|> (try $ string "stop" >> return Stop)
-
-parseRules :: String -> Either ParseError Rules
-parseRules input = parse rules "(unknown)" input
-
-continue :: RuleType -> Bool
-continue Continue = True
-continue ContInt = True
-continue _ = False
-
-protect :: RuleType -> Bool
-protect Protect = True
-protect ProtInt = True
-protect _ = False
-
-intact :: RuleType -> Bool
-intact Intact = True
-intact ProtInt = True
-intact ContInt = True
+parseRule :: GenParser Char st Rule
+parseRule = do
+  suffix <- many1 letter
+  skipCount <- many1 digit
+  -- TODO MIGHT be a * here.
+  action <- oneOf ">."
+  _ <- oneOf "\n"
+  let ruleType = if action == '>'
+                 then Continue
+                 else Stop
+  return (Rule suffix (read skipCount) False ruleType)
 
 isVowel :: Char -> Bool
 isVowel = flip elem "aeiou"
@@ -86,8 +47,8 @@ isConsonant s = isLower s && not (isVowel s)
 -- |one of which must be a consonant.  If however, it beings with a vowel then
 -- |it must contain three letters and at least one of these must be a vowel
 -- |or a 'y'
-acceptable :: String -> Bool
-acceptable s = len > 3 && 
+isAcceptable :: String -> Bool
+isAcceptable s = len > 3 && 
                (fstIsVowel && sndIsConsonant) &&
                (sndIsVowel || thirdIsVowel)
     where
@@ -98,25 +59,31 @@ acceptable s = len > 3 &&
       sndIsConsonant = len >= 2 && isConsonant sndChar
       thirdIsVowel = len >= 3 && isVowel (s !! 2)
 
--- |Apply the rule giving the result together with the stemmed word
-applyRule :: Rule -> String -> Bool -> (String,RuleResult)
-applyRule r@(Rule keyString replaceString ruleType) word itct | shouldBeIntact || (not endingMatches) = (word,RuleNotApplied)
-                                                              | (protect ruleType) = (word,RuleStop)
-                                                              | continue ruleType = (replaceString,RuleContinue)
-                                                              | otherwise = (replaceString,RuleStop)
+firstMatchRule :: String -> [Rule] -> Maybe Rule
+firstMatchRule word rules = find (ruleMatches word) rules
+
+ruleMatches :: String -> Rule -> Bool
+ruleMatches word (Rule suffix _ _ _) = suffix `isSuffixOf` word
+
+stem :: String -> [Rule] -> String
+stem word [] = word
+stem word rules | Nothing == matchingRule = word
+                | otherwise = undefined
     where
-      shouldBeIntact = itct && (not $ intact ruleType)
-      endingMatches = keyString `isSuffixOf` word
+      matchingRule = firstMatchRule word rules
+      (Rule suffix drop intact action) = fromJust matchingRule
+  _ <- many (noneOf "\n")
+  return (Rule keyString replaceString ruleType)
 
-applyRules :: Rules -> String -> String
-applyRules r s = undefined where
-    rules = Map.findWithDefault [] (last s) r
+-- TODO tomorrow actually compile
 
+{-
 main = do
   args <- getArgs
-  let inputFile = args !! 0
+  let inputFile = head args
       outputFile = args !! 1
   rulesString <- readFile inputFile
   let (Right rules) = parseRules rulesString
   print rules
   return ()
+-}
