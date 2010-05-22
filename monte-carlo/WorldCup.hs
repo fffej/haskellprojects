e26f2dda060ed9ec18311d41809d63ca4bb43f7e
Jeff Foster <jfoster@acm.org>
2010-05-22 12:11:54 +0100

    Deterministic version of the world cup.  Unsuprisingly Brazil are going to win.

--- monte-carlo/WorldCup.hs
+++ monte-carlo/WorldCup.hs
@@ -1,12 +1,12 @@
 module WorldCup where
 
 import Data.Maybe (fromJust)
-import Data.List ((!!))
+import Data.List ((!!),sortBy)
+import qualified Data.Map as Map
 
 type Ranking = Int
 
-data LeagueEntry = LeagueEntry Team Int
-                   deriving (Show,Eq)
+type League = Map.Map Team Int
 
 data GameResult = Win | Lose | Draw
                   deriving (Show,Eq)
@@ -19,48 +19,33 @@ data Team = RSA | MEX | URA | FRA |
             ITA | PAR | NZL | SVK | 
             BRA | PRK | CIV | POR | 
             ESP | SUI | HON | CHI
-            deriving (Show,Eq)
+            deriving (Show,Eq,Ord)
 
 data GroupName = A | B | C | D | E | F | G | H
-                 deriving (Show)
+                 deriving (Show,Eq,Enum)
 
 data Group = Group {
       group :: GroupName
-    , teams :: [Team]
-    , league :: [LeagueEntry]
+    , teams :: (Team,Team,Team,Team)
 } deriving (Show)
 
-data WorldCup = WorldCup [Group]
+data WorldCup = WorldCup [Group] deriving (Show)
 
-data KnockoutStage = KnockoutStage [Team]
+data KnockoutStage = KnockoutStage [Team] deriving (Show)
 
 -- TODO provide a sensible way of winning
 play :: Team -> Team -> GameResult
-play _ _ = Win
+play x y | r1 > r2 = Win
+         | r2 > r1 = Lose
+         | r1 == r2 = Lose
+    where
+      r1 = fromJust $ lookup x rankings28April
+      r2 = fromJust $ lookup y rankings28April
 
 winner :: Team -> Team -> Team
 winner x y | play x y == Win = x
-winner x y | play x y == Lose = x
+winner x y | play x y == Lose = y
 winner x y | play x y == Draw = winner x y
-
--- | (NameOfWinner, ((Position,Group) vs (Position, Group)))
-last16 :: [(Int,((Int,GroupName),(Int,GroupName)))]
-last16 = [(2,((1,B),(2,A)))
-         ,(7,((1,G),(2,H)))
-         ,(3,((1,C),(2,D)))
-         ,(4,((1,D),(2,C)))
-         ,(8,((1,H),(2,G)))
-         ,(5,((1,E),(2,F)))
-         ,(1,((1,A),(2,B)))
-         ,(6,((1,F),(2,E)))]
-
-last8 :: [(Char,(Int,Int))]
-last8 = [('A', (1,3))
-        ,('D', (6,8))
-        ,('B', (2,4))
-        ,('C', (5,7))]
-
-last4 = [('A','C'),('B','D')]
 
 -- |Simulate the world cup
 rankings28April :: [(Team,Ranking)]
@@ -76,23 +61,61 @@ rankings28April =
      (ESP,1565), (SUI,854), (HON,727), (CHI,948)
     ]
 
-makeGroup :: GroupName -> [Team] -> Group
-makeGroup name teams = Group name teams leagues where
-    leagues = map (\x -> LeagueEntry x 0) teams
+makeGroup :: GroupName -> (Team,Team,Team,Team) -> Group
+makeGroup name t@(a,b,c,d) = Group name t
 
-groupA = makeGroup A [RSA, MEX, URA, FRA]
-groupB = makeGroup B [ARG, NGA, KOR, GRE]
-groupC = makeGroup C [ENG, USA, ALG, SVN]
-groupD = makeGroup D [GER, AUS, SRB, GHA]
-groupE = makeGroup E [NED, DEN, JPN, CMR]
-groupF = makeGroup F [ITA, PAR, NZL, SVK]
-groupG = makeGroup G [BRA, PRK, CIV, POR]
-groupH = makeGroup H [ESP, SUI, HON, CHI]
+groupA = makeGroup A (RSA, MEX, URA, FRA)
+groupB = makeGroup B (ARG, NGA, KOR, GRE)
+groupC = makeGroup C (ENG, USA, ALG, SVN)
+groupD = makeGroup D (GER, AUS, SRB, GHA)
+groupE = makeGroup E (NED, DEN, JPN, CMR)
+groupF = makeGroup F (ITA, PAR, NZL, SVK)
+groupG = makeGroup G (BRA, PRK, CIV, POR)
+groupH = makeGroup H (ESP, SUI, HON, CHI)
 
-worldCup = WorldCup [groupA,groupB,groupC,groupD,groupE,groupF,groupG,groupH]
+
+worldCup = WorldCup [
+            groupA
+           ,groupB
+           ,groupC
+           ,groupD
+           ,groupE
+           ,groupF
+           ,groupG
+           ,groupH
+           ]
+
+scoreGame :: League -> ((Team,Team),GameResult) -> League
+scoreGame r ((x,y),Win) = Map.insertWith (+) x 3 r
+scoreGame r ((x,y),Lose) = Map.insertWith (+) y 3 r
+scoreGame r ((x,y),Draw) = Map.insertWith (+) y 1 (Map.insertWith (+) x 1 r)
+
+scoreGames :: League -> [((Team,Team),GameResult)] -> League
+scoreGames = foldl scoreGame 
+
+fixtures :: (Team,Team,Team,Team) -> [(Team,Team)]
+fixtures (a,b,c,d) = [(a,b),(a,c),(a,d),(b,c),(b,d),(c,d)]
+
+initialLeague :: (Team,Team,Team,Team) -> League
+initialLeague (a,b,c,d) = Map.fromList [(a,0),(b,0),(c,0),(d,0)]
+
+playGroup :: Group -> League
+playGroup (Group _ t) = scoreGames (initialLeague t) (zip (fixtures t) results) where
+    results = map (uncurry play) (fixtures t) :: [GameResult]    
+
+lookupPosition :: [(GroupName,League)] -> (GroupName,Int) -> Team
+lookupPosition s (n,x) | x == 1 = fst $ head sortedList
+                       | x == 2 = fst $ head $ tail sortedList
+    where
+      l = Map.toList $ fromJust (lookup n s)
+      sortedList = sortBy (\(_,x) (_,y) -> compare y x) l
 
 advanceToKnockOut :: WorldCup -> KnockoutStage
-advanceToKnockOut (WorldCup groups) = undefined
+advanceToKnockOut (WorldCup groups) = KnockoutStage teams where
+    groupWinners = zip [A .. H] (map playGroup groups) :: [(GroupName,League)]
+    rules = [(A,1),(F,1),(B,1),(E,1),(C,1),(H,1),(D,1),(G,1),
+             (B,2),(E,2),(A,2),(F,2),(D,2),(G,2),(C,2),(H,2)]
+    teams = map (lookupPosition groupWinners) rules
 
 nextRound :: KnockoutStage -> KnockoutStage
 nextRound (KnockoutStage (x:[])) = KnockoutStage [x]
@@ -105,4 +128,4 @@ simulate :: WorldCup -> Team
 simulate wc = head x where
     knockOut = advanceToKnockOut wc
     rounds = iterate nextRound knockOut
-    KnockoutStage x = rounds !! 5
+    KnockoutStage x = rounds !! 4
