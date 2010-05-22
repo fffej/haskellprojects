94378067866b5a8b701c1113068d4d6f81580f62
Jeff Foster <jfoster@acm.org>
2010-05-22 12:43:41 +0100

    Parameterize by a function.  Should be able to use big list of random numbers instead of worrying about monads.

--- monte-carlo/WorldCup.hs
+++ monte-carlo/WorldCup.hs
@@ -1,7 +1,7 @@
 module WorldCup where
 
 import Data.Maybe (fromJust)
-import Data.List ((!!),sortBy)
+import Data.List (sortBy)
 import qualified Data.Map as Map
 
 type Ranking = Int
@@ -24,10 +24,7 @@ data Team = RSA | MEX | URA | FRA |
 data GroupName = A | B | C | D | E | F | G | H
                  deriving (Show,Eq,Enum)
 
-data Group = Group {
-      group :: GroupName
-    , teams :: (Team,Team,Team,Team)
-} deriving (Show)
+data Group = Group GroupName (Team,Team,Team,Team) deriving (Show)
 
 data WorldCup = WorldCup [Group] deriving (Show)
 
@@ -35,17 +32,22 @@ data KnockoutStage = KnockoutStage [Team] deriving (Show)
 
 -- TODO provide a sensible way of winning
 play :: Team -> Team -> GameResult
-play x y | r1 > r2 = Win
-         | r2 > r1 = Lose
-         | r1 == r2 = Lose
+play x y = case result of
+             GT -> Win
+             LT -> Lose
+             EQ -> Lose
     where
       r1 = fromJust $ lookup x rankings28April
       r2 = fromJust $ lookup y rankings28April
+      result = compare r1 r2
 
 winner :: Team -> Team -> Team
-winner x y | play x y == Win = x
-winner x y | play x y == Lose = y
-winner x y | play x y == Draw = winner x y
+winner x y = case result of
+               Win -> x
+               Lose -> y
+               Draw -> winner x y
+    where
+      result = play x y
 
 -- |Simulate the world cup
 rankings28April :: [(Team,Ranking)]
@@ -62,18 +64,33 @@ rankings28April =
     ]
 
 makeGroup :: GroupName -> (Team,Team,Team,Team) -> Group
-makeGroup name t@(a,b,c,d) = Group name t
+makeGroup = Group
 
+groupA :: Group
 groupA = makeGroup A (RSA, MEX, URA, FRA)
+
+groupB :: Group
 groupB = makeGroup B (ARG, NGA, KOR, GRE)
+
+groupC :: Group
 groupC = makeGroup C (ENG, USA, ALG, SVN)
+
+groupD :: Group
 groupD = makeGroup D (GER, AUS, SRB, GHA)
+
+groupE :: Group
 groupE = makeGroup E (NED, DEN, JPN, CMR)
+
+groupF :: Group
 groupF = makeGroup F (ITA, PAR, NZL, SVK)
+
+groupG :: Group
 groupG = makeGroup G (BRA, PRK, CIV, POR)
+
+groupH :: Group
 groupH = makeGroup H (ESP, SUI, HON, CHI)
 
-
+worldCup :: WorldCup
 worldCup = WorldCup [
             groupA
            ,groupB
@@ -86,8 +103,8 @@ worldCup = WorldCup [
            ]
 
 scoreGame :: League -> ((Team,Team),GameResult) -> League
-scoreGame r ((x,y),Win) = Map.insertWith (+) x 3 r
-scoreGame r ((x,y),Lose) = Map.insertWith (+) y 3 r
+scoreGame r ((x,_),Win) = Map.insertWith (+) x 3 r
+scoreGame r ((_,y),Lose) = Map.insertWith (+) y 3 r
 scoreGame r ((x,y),Draw) = Map.insertWith (+) y 1 (Map.insertWith (+) x 1 r)
 
 scoreGames :: League -> [((Team,Team),GameResult)] -> League
@@ -99,33 +116,36 @@ fixtures (a,b,c,d) = [(a,b),(a,c),(a,d),(b,c),(b,d),(c,d)]
 initialLeague :: (Team,Team,Team,Team) -> League
 initialLeague (a,b,c,d) = Map.fromList [(a,0),(b,0),(c,0),(d,0)]
 
-playGroup :: Group -> League
-playGroup (Group _ t) = scoreGames (initialLeague t) (zip (fixtures t) results) where
-    results = map (uncurry play) (fixtures t) :: [GameResult]    
+playGroup :: (Team -> Team -> GameResult) -> Group -> League
+playGroup resultFunc (Group _ t) = scoreGames (initialLeague t) (zip (fixtures t) results) 
+    where
+      results = map (uncurry resultFunc) (fixtures t) :: [GameResult]    
 
 lookupPosition :: [(GroupName,League)] -> (GroupName,Int) -> Team
 lookupPosition s (n,x) | x == 1 = fst $ head sortedList
                        | x == 2 = fst $ head $ tail sortedList
+                       | otherwise = error "Invalid rules for looking up groups"
     where
       l = Map.toList $ fromJust (lookup n s)
-      sortedList = sortBy (\(_,x) (_,y) -> compare y x) l
+      sortedList = sortBy (\(_,a) (_,b) -> compare b a) l
 
-advanceToKnockOut :: WorldCup -> KnockoutStage
-advanceToKnockOut (WorldCup groups) = KnockoutStage teams where
-    groupWinners = zip [A .. H] (map playGroup groups) :: [(GroupName,League)]
+advanceToKnockOut :: WorldCup -> (Team -> Team -> GameResult) -> KnockoutStage
+advanceToKnockOut (WorldCup groups) playFunc = KnockoutStage teams where
+    groupWinners = zip [A .. H] (map (playGroup playFunc) groups) :: [(GroupName,League)]
     rules = [(A,1),(F,1),(B,1),(E,1),(C,1),(H,1),(D,1),(G,1),
              (B,2),(E,2),(A,2),(F,2),(D,2),(G,2),(C,2),(H,2)]
     teams = map (lookupPosition groupWinners) rules
 
-nextRound :: KnockoutStage -> KnockoutStage
-nextRound (KnockoutStage (x:[])) = KnockoutStage [x]
-nextRound (KnockoutStage teams) = KnockoutStage results where
+nextRound :: (Team -> Team -> Team) -> KnockoutStage -> KnockoutStage
+nextRound _ (KnockoutStage (x:[])) = KnockoutStage [x]
+nextRound winFunc (KnockoutStage teams) = KnockoutStage results where
     len = length teams `div` 2
     matchUps = uncurry zip $ splitAt len teams
-    results = map (uncurry winner) matchUps
+    results = map (uncurry winFunc) matchUps
 
-simulate :: WorldCup -> Team
-simulate wc = head x where
-    knockOut = advanceToKnockOut wc
-    rounds = iterate nextRound knockOut
+-- simulate worldCup (play) (winner)
+simulate :: WorldCup -> (Team -> Team -> GameResult) -> (Team -> Team -> Team) -> Team
+simulate wc leagueFunc winFunc = head x where
+    knockOut = advanceToKnockOut wc leagueFunc
+    rounds = iterate (nextRound winFunc) knockOut
     KnockoutStage x = rounds !! 4
