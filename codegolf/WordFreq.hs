9a2f07418c0ef06c2ec327113e2666ddec813aac
Jeff Foster <jfoster@acm.org>
2010-07-03 20:36:05 +0100

    Without any minification

---
+++ codegolf/WordFreq.hs
@@ -0,0 +1,39 @@
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.List (sortBy)
+
+ignoredWords :: [String]
+ignoredWords = words "the and of to a i it in or is"
+
+type SSMap = Map String Int
+
+countInMap :: SSMap -> String -> SSMap
+countInMap m k = M.insertWith (+) k 1 m
+
+countWords :: String -> [(String,Int)]
+countWords s = take 22 $ sortList $ foldl countInMap M.empty w
+    where 
+      w = filter (not . (`elem` ignoredWords)) (words s)
+
+sortList :: SSMap -> [(String,Int)]
+sortList m = sortBy (\(_,x) (_,y) -> compare y x) (M.toList m)
+
+maxLength :: [(String,Int)] -> Int
+maxLength = snd . head
+
+draw :: [(String,Int)] -> String
+draw w = ' ' : h ++ concatMap (drawItem ww) w
+    where
+      n = (snd . head) w -- max length
+      lw = foldl1 max (map (length . fst) w) -- longest word
+      wi = 80 - (lw + n)
+      ww = wi `div` n
+      h = take (ww * n) (repeat '_') ++ "\n"
+
+drawItem :: Int -> (String,Int) -> String
+drawItem ww (w,n) = "|" ++ (take x (repeat '_')) ++ "| " ++ w ++ "\n"
+    where
+      x = n * ww
+
+main :: IO ()
+main = interact (draw . countWords)
