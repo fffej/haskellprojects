fffe23c991ef61897687380ec08bb58c2392382e
Jeff Foster <jfoster@acm.org>
2010-07-03 20:53:59 +0100

    hlint simplifications

--- codegolf/WordFreq.hs
+++ codegolf/WordFreq.hs
@@ -2,28 +2,38 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Data.List (sortBy)
 
+ignoredWords :: [String]
 ignoredWords = words "the and of to a i it in or is"
 
+type SSMap = Map String Int
+
+countInMap :: SSMap -> String -> SSMap
 countInMap m k = M.insertWith (+) k 1 m
 
+countWords :: String -> [(String,Int)]
 countWords s = take 22 $ sortList $ foldl countInMap M.empty w
     where 
       w = filter (not . (`elem` ignoredWords)) (words s)
 
+sortList :: SSMap -> [(String,Int)]
 sortList m = sortBy (\(_,x) (_,y) -> compare y x) (M.toList m)
 
+maxLength :: [(String,Int)] -> Int
 maxLength = snd . head
 
-draw w = (' ' : h ++ concatMap (drawItem ww) w)
+draw :: [(String,Int)] -> String
+draw w = ' ' : h ++ concatMap (drawItem ww) w
     where
       n = (snd . head) w -- max occurences of the term 
       lw = foldl1 max (map (length . fst) w) -- longest word
       wi = 80 - (lw + 3) -- wi is the number of characters we have to fill
-      ww = ((realToFrac wi) / (realToFrac n)) :: Double -- width of each occurence should be max occurences
-      h = take (round (ww * realToFrac n)) (repeat '_') ++ "\n"
+      ww = realToFrac wi / realToFrac n :: Double -- width of each occurence should be max occurences
+      h = replicate (round (ww * realToFrac n)) '_' ++ "\n"
 
-drawItem ww (w,n) = "|" ++ (take x (repeat '_')) ++ "| " ++ w ++ "\n"
+drawItem :: Double -> (String,Int) -> String
+drawItem ww (w,n) = "|" ++ replicate x '_' ++ "| " ++ w ++ "\n"
     where
-      x = round ((realToFrac n) * ww)
+      x = round (realToFrac n * ww)
 
+main :: IO ()
 main = interact (draw . countWords)
