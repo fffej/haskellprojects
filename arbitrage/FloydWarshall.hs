571b68193b7e925149e5b9e946a51776fd212708
Jeff Foster <jfoster@acm.org>
2010-07-06 08:04:38 +0100

    Get rid of daft extra loops.  WTF was I thinking?

--- arbitrage/FloydWarshall.hs
+++ arbitrage/FloydWarshall.hs
@@ -29,52 +29,55 @@ infinity = 1000000
 
 createArray :: Int -> IO Array
 createArray n = do
+  print n
   let n2 = n*n 
-  v <- GM.unsafeNewWith n2 0
-  p <- GM.unsafeNewWith n2 0
-  return (Array n2 v p)
+  v <- GM.newWith n2 0
+  p <- GM.newWith n2 0
+  return (Array n v p)
 
 initializeArray :: (Graph a b) => a -> IO Array
 initializeArray g = do
   arr <- createArray (length (vertices g))
-  let n = length $ vertices g
+  let n = (length $ vertices g) - 1
   forM_ [0..n]
         (\i -> forM_ [0..n]
-         (\j -> forM_ [0..n]
-          (\_ -> do
-             let w = edge g (fromInt g i) (fromInt g j)
-                 v = maybe infinity (const (fromJust w)) w
-             writeVal arr (i,j) 0.0))) -- TODO lookup the weight
+         (\j -> do 
+          let w = edge g (fromInt g i) (fromInt g j)
+              v = maybe infinity (const (fromJust w)) w
+          writeVal arr (i,j) v)) -- TODO lookup the weight
   return arr
 
 floydWarshall :: Graph a b => a -> Array -> IO ()
 floydWarshall g arr = do
-  let n = length $ vertices g
+  let n = (length $ vertices g) - 1
   forM_ [0..n]
         (\k -> forM_ [0..n]
          (\i -> forM_ [0..n]
-          (\j -> forM_ [0..n]
-           (\_ -> do
-              ij <- readVal arr (i,j)
-              ikkj <- liftM2 (+) (readVal arr (i,k)) (readVal arr (k,j))
-              _ <- if (ikkj < ij) then (writeVal arr (i,j) ikkj) else (writePath arr (i,j) (fromIntegral k))
-              return ()))))
+          (\j -> do
+             ij <- readVal arr (i,j)
+             ikkj <- liftM2 (+) (readVal arr (i,k)) (readVal arr (k,j))
+             _ <- if (ikkj < ij) then (writeVal arr (i,j) ikkj) else (writePath arr (i,j) (fromIntegral k))
+             return ())))
 
-reconstructPath :: Graph a b => a -> Array -> IO [b]
-reconstructPath = undefined
+printArray :: Graph a b => a -> Array -> IO ()
+printArray g arr = do
+  let n = (length $ vertices g) - 1
+  forM_ [0..n]
+        (\i -> forM_ [0..n]
+         (\j -> print (i,j)))
 
 ix :: Int -> (Int,Int) -> Int
 ix n (i,j) = i*n + j
 
 writeVal :: Array -> (Int,Int) -> Double -> IO ()
-writeVal (Array n vec _) p = GM.unsafeWrite vec (ix n p)
+writeVal (Array n vec _) p = GM.write vec (ix n p)
 
 writePath :: Array -> (Int,Int) -> Double -> IO ()
-writePath (Array n _ vec) p = GM.unsafeWrite vec (ix n p)
+writePath (Array n _ vec) p = GM.write vec (ix n p)
 
 readVal :: Array -> (Int,Int) -> IO Double 
-readVal (Array n vec _) p = GM.unsafeRead vec (ix n p)
+readVal (Array n vec _) p = GM.read vec (ix n p)
 
 readPath :: Array -> (Int,Int) -> IO Double
-readPath (Array n _ vec) p = GM.unsafeRead vec (ix n p)
+readPath (Array n _ vec) p = GM.read vec (ix n p)
 
