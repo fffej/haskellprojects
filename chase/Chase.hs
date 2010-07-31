94fad44221a92f06d870f392b3c7926bed16b9a8
Jeff Foster <jfoster@acm.org>
2010-07-31 23:07:15 +0100

    Be able to draw the environment.

--- chase/Chase.hs
+++ chase/Chase.hs
@@ -3,13 +3,13 @@ module Chase where
 import Data.Map (Map)
 import qualified Data.Map as M
 
-import Data.Maybe (catMaybes)
+import Data.Maybe (mapMaybe)
 
 -- Colloborate Diffusion
+-- http://en.wikipedia.org/wiki/Antiobjects
 type Desirability = Double
 type Scent = Double
 type Point = (Int,Int)
-type AgentStack = [Agent]
 
 diffusionRate :: Double
 diffusionRate = 0.25
@@ -19,16 +19,31 @@ data Agent = Goal Desirability
            | Path Scent
            | Obstacle
 
+-- |An attempt to enforce that this can't be empty
+data AgentStack = AgentStack {
+      top :: Agent
+    , rest :: [Agent]
+}
+
 data Environment = Environment {
       board :: Map Point AgentStack
+    , size :: Int
 }
+
+createEnvironment size = Environment b size
+    where
+      b = M.fromList [((x,y),mkAgent x y) | x <- [0..size], y <- [0..size] ]
+      mkAgent x y | x == 0 || y == 0 || x == size || y == size = AgentStack Obstacle []
+                  | x == (size `div` 2) && y == (size `div` 2) = AgentStack (Goal 100) []
+                  | x == 1 && y == 1 = AgentStack (Pursuer 0) []
+                  | x == (size - 1) && y == (size - 1) = AgentStack (Pursuer 0) []
+                  | otherwise = AgentStack (Path 0) []
 
 
 diffuse :: Environment -> Environment
 diffuse = undefined
 
-neighbours :: Environment -> Point -> (Agent,Agent,Agent,Agent)
-neighbours (Environment e) (x,y) = undefined
+neighbours :: Environment -> Point -> [Agent]
+neighbours (Environment e _) (x,y) = map top $ mapMaybe (`M.lookup` e) n
     where
-      n = [ (x+dx,y+dy)  | (dx,dy) <- [(1,0),(-1,0),(1,0),(0,1)]] 
-      c = catMaybes $ map ((flip M.lookup) e) n
+      n = [ (x+dx,y+dy) | (dx,dy) <- [(1,0),(-1,0),(1,0),(0,1)]] 
---
+++ chase/ChaseVis.hs
@@ -0,0 +1,98 @@
+module ChaseVis where
+
+import Chase
+
+import Graphics.UI.GLUT as G
+import System.Exit (exitWith, ExitCode(ExitSuccess))
+import Control.Monad (unless, when, forM_,liftM,liftM2)
+import Data.IORef (IORef, newIORef)
+
+import Data.Map (Map,(!))
+import qualified Data.Map as M
+
+data State = State {
+      env :: IORef Environment
+}
+
+makeState :: IO State
+makeState = liftM State (newIORef $ createEnvironment 32)
+
+winHeight :: Int
+winHeight = 512
+
+winWidth :: Int
+winWidth = 512
+
+tick :: Int
+tick = 25
+
+color3f :: Color3 GLfloat -> IO ()
+color3f = color
+
+vertex2f :: Vertex2 GLfloat -> IO ()
+vertex2f = vertex :: Vertex2 GLfloat -> IO ()
+
+colorVertex :: Color3 GLfloat ->  Vertex2 GLfloat -> IO ()  
+colorVertex c v = color3f c >> vertex v
+
+displayFunc :: State -> DisplayCallback
+displayFunc s = do
+  clear [ColorBuffer]
+  e <- G.get (env s)
+  _ <- drawGrid e 
+  flush
+  swapBuffers
+
+pickColor :: Agent -> Color3 GLfloat
+pickColor (Goal x) = Color3 1 0 0
+pickColor (Pursuer s) = Color3 0 1 0
+pickColor (Path s) = Color3 0 0 1
+pickColor Obstacle = Color3 1 1 1
+
+drawGrid :: Environment -> IO ()
+drawGrid (Environment g b) = do
+  lineWidth $= realToFrac 0.1
+  let sqSize = (fromIntegral winHeight / fromIntegral b)
+  let f i = ((fromIntegral i - 0.5 :: GLfloat) * sqSize)
+  renderPrimitive Quads $ forM_ [(x,y) | x <- [0..b], y <- [0..b]]
+                      (\(i,j) ->
+                             do
+                               let c = pickColor (top $ g ! (i,j))
+                               colorVertex c (Vertex2 (f i) (f j))
+                               colorVertex c (Vertex2 (f i+sqSize) (f j))
+                               colorVertex c (Vertex2 (f i+sqSize) (f j+sqSize))
+                               colorVertex c (Vertex2 (f i) (f j+sqSize)))
+  flush
+    
+
+timerFunc :: State -> IO ()
+timerFunc s = do
+  addTimerCallback tick (timerFunc s)
+
+reshapeFunc :: ReshapeCallback
+reshapeFunc size@(Size _ height) = 
+    unless (height == 0) $ do
+      viewport $= (Position 0 0, size)
+      loadIdentity
+      ortho2D 0 512 0 512
+      clearColor $= Color4 0 0 0 1
+
+keyboardMouseHandler :: State -> KeyboardMouseCallback
+keyboardMouseHandler  _ _ _ _ _ = return ()
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  initialDisplayMode $= [ DoubleBuffered, RGBAMode ]
+  initialWindowSize $= Size 512 512
+  initialWindowPosition $= Position 0 0
+  _ <- createWindow "Agent Visualization"
+
+  state <- makeState
+
+  displayCallback $= displayFunc state
+  reshapeCallback $= Just reshapeFunc
+  keyboardMouseCallback $= Just (keyboardMouseHandler state)
+  addTimerCallback tick (timerFunc state)
+
+  mainLoop
