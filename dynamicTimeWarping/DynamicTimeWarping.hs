09a2eb2aabf69980668175ed57bfb6281541439f
Jeff Foster <jeff.foster@acm.org>
2014-07-17 20:01:48 +0100

    fix array index out of bounds runtime error

--- dynamicTimeWarping/DynamicTimeWarping.hs
+++ dynamicTimeWarping/DynamicTimeWarping.hs
@@ -25,8 +25,8 @@ dtw x y cost = runSTArray $ do
   d <- newArray ((0,0),(m,n)) 0
   forM_ [1..n] (\i -> writeArray d (0,i) maxcost)
   forM_ [1..m] (\i -> writeArray d (i,0) maxcost)
-  forM_ [1..m] $ \i -> 
-    forM_ [1..n] $ \j -> do
+  forM_ [1..n] $ \i -> 
+    forM_ [1..m] $ \j -> do
       let c = cost (x ! (i -1)) (y ! (j -1))
       insertion <- readArray d (j,i-1)
       deletion <- readArray d (j-1,i)
@@ -35,17 +35,12 @@ dtw x y cost = runSTArray $ do
   return d
 
 render :: Array (Int,Int) Int -> FilePath -> IO ()
-render arr file = do
-  print w
-  print h
-  print (length (elems arr))
-  print (BS.length bs)
-  writeBMP file bmp
-    where
-      ((_,_),(w,h)) = bounds arr
-      bs = BS.pack (concatMap (normalize 0 (maximum vs)) vs)
-      bmp = packRGBA32ToBMP w h bs
-      vs = map snd $ filter (\((x,y),_) -> x /= 0 && y /= 0) (assocs arr)
+render arr file = writeBMP file bmp
+  where
+    ((_,_),(w,h)) = bounds arr
+    bs = BS.pack (concatMap (normalize 0 (maximum vs)) vs)
+    bmp = packRGBA32ToBMP w h bs
+    vs = map snd $ filter (\((x,y),_) -> x /= 0 && y /= 0) (assocs arr)
 
 -- TODO http://stackoverflow.com/questions/7706339/grayscale-to-red-green-blue-matlab-jet-color-scale
 normalize :: Int -> Int -> Int -> [Word8]
@@ -53,10 +48,10 @@ normalize minx maxx x = [scale r, scale g, scale b, 0]
   where
     (r,g,b) = color normalized
     scale v = floor (maxB * v)
-    normalized = delta / range
+    normalized = delta / rnge
     maxB = fromIntegral (maxBound :: Word8)
     delta = fromIntegral $ x - minx
-    range = fromIntegral $ maxx - minx
+    rnge = fromIntegral $ maxx - minx
 
 -- v is bound between 0 and 1
 -- dv is 1
@@ -68,9 +63,13 @@ color v
   | otherwise = (1,1 + 4 * (0.75 - v),1)
     
 
-example1 :: IO ()
-example1 = do
-  let seq1 = [0 .. 500]
-      seq2 = [0 .. 500]
-      cost = dtw (V.fromList seq1) (V.fromList seq2) intCost
-  render cost "exact.bmp"
+save :: [Int] -> [Int] -> FilePath -> IO ()
+save seq1 seq2 filename = do
+  let cost = dtw (V.fromList seq1) (V.fromList seq2) intCost
+  render cost filename
+
+main :: IO ()
+main = do
+  save [0..500] [0..500] "perfect.bmp"
+  save [0..500] [500,499..0] "opposite.bmp"
+  save [0..500] [2,4..1000] "double.bmp"
