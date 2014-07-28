abb60dad24e65bf732e00e30d6ee57b411ea1a9d
Jeff Foster <jeff.foster@acm.org>
2014-07-28 19:13:27 +0100

    more eigen faces

---
+++ principalCompnentAnalysis/Biometrics/Face/EigenFaces.hs
@@ -0,0 +1,6 @@
+module Biometrics.Face.EigenFaces where
+
+import Numeric.Statistics.PCA
+
+hello :: String
+hello = "hello"
---
+++ principalCompnentAnalysis/Biometrics/Face/EigenFaces_flymake.hs
@@ -0,0 +1,6 @@
+module Biometrics.Face where
+
+import Numeric.Statistics.PCA
+
+hello :: String
+hello = "hello"
