f3b9fe741cf173618f7cea5e1f1480d93f85d5d3
Jeff Foster <jeff.foster@acm.org>
2014-07-30 15:59:46 +0100

    convert over to right format, pca works in repl, now to visualize

--- PCA/Biometrics/Face/EigenFaces_flymake.hs
+++ PCA/Biometrics/Face/EigenFaces_flymake.hs
@@ -1,7 +1,19 @@
 module Biometrics.Face.EigenFaces where
 
+import Biometrics.Face.FaceDatabase (faceDatabase)
 import Numeric.Statistics.PCA
 import Codec.BMP
 
-hello :: String
-hello = "hello"
+import Data.Vector hiding (mapM,map)
+
+import Control.Monad (liftM)
+import Data.Either (rights)
+
+readDatabase :: IO [BMP]
+readDatabase = liftM rights $ mapM readBMP faceDatabase
+
+convert :: [BMP] -> [Vector Double]
+convert = map toVector
+
+toVector :: BMP -> Vector Double
+toVector = undefined
