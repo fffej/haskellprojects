579975d73d10edbb6a06adda8dd09cf5670dff20
Jeff Foster <jeff.foster@acm.org>
2015-01-02 15:15:01 +0000

    trying (and failing) to gain intuition for lens

--- diamond-square/src/Main.hs
+++ diamond-square/src/Main.hs
@@ -1,9 +1,38 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
-import Diagrams.Prelude
+import Diagrams.Prelude hiding (Point)
 import Diagrams.Backend.SVG.CmdLine
 
+import Control.Lens
+
+data Point = Point Int Int deriving (Show,Eq)
+
+data Square = Square
+              {
+                _topLeft :: Point                
+              , _size    :: Int -- TODO enforce only 2^x
+              , _height  :: Int
+              } deriving (Show,Eq)
+
+makeLenses ''Square
+
+addPoint :: Point -> Point -> Point
+addPoint (Point x y) (Point a b) = Point (a+x) (b+y)
+
+move :: Point -> Square -> Square
+move p = topLeft `over` (addPoint p)
+
+divide :: Square -> [Square]
+divide sq = [topLeft,topRight,bottomLeft,bottomRight]
+  where
+    topLeft = undefined -- same top left, size / 2
+    topRight = undefined
+    bottomLeft = undefined 
+    bottomRight = undefined
+    newSize = size `over` (`div` 2) $ sq
+
 main :: IO ()
-main = mainWith (circle 1 :: Diagram B R2)
+main = undefined
