4ac1bef3277f592d7d12bf0c00b78cbb7db1c3c6
Jeff Foster <jeff.foster@acm.org>
2015-01-06 14:29:37 +0000

    almost works?

--- diamond-square/src/Main.hs
+++ diamond-square/src/Main.hs
@@ -7,6 +7,9 @@ import Control.Lens
 import Codec.Picture
 import qualified Data.Map.Strict as M
 
+import System.Random
+import Control.Monad (liftM)
+
 type Point = (Int,Int)
 
 data Square = Square
@@ -34,7 +37,7 @@ move :: Point -> Square -> Square
 move p = position `over` addPoint p
 
 averageHeight :: Double -> Square -> Double
-averageHeight eps sq = eps + ((sq^.tl + sq^.tr + sq^.bl + sq ^.br) / 4.0)
+averageHeight eps sq = abs $ eps + ((sq^.tl + sq^.tr + sq^.bl + sq ^.br) / 4.0)
 
 averageTopHeight :: Square -> Double
 averageTopHeight sq = (sq^.tl + sq^.tr) / 2.0 
@@ -61,19 +64,35 @@ allSubSquares f sq
   | isUnit sq = [sq]
   | otherwise = concatMap (allSubSquares f) (f 0 sq)
 
+allSubSquaresWithRandomPerturbation :: (Double -> Square -> [Square]) -> Square -> IO [Square]
+allSubSquaresWithRandomPerturbation f sq
+  | isUnit sq = return [sq]
+  | otherwise = do
+    x <- randomRIO (-(0.1),0.1)
+    liftM concat $ mapM (allSubSquaresWithRandomPerturbation f) (f x sq)
+    
+
 imageSize :: Int
 imageSize = 256
+
+scale :: Double -> Pixel16
+scale p = truncate (65536 * p)
 
 generatePlasma :: Square -> Image Pixel16
 generatePlasma sq = generateImage f imageSize imageSize
   where
-    scale p = truncate (65536 * p)
     f x y = scale (M.findWithDefault 0 (x,y) pixels) 
     pixels = M.fromList $ map (\x -> (x^.position, averageHeight 0 x)) $ allSubSquares divide sq
 
+generatePlasma2 :: Square -> IO (Image Pixel16)
+generatePlasma2 sq = do
+  sqs <- allSubSquaresWithRandomPerturbation divide sq
+  let f x y = scale (M.findWithDefault 0 (x,y) pixels)
+      pixels = M.fromList $ map (\x -> (x^.position, averageHeight 0 x)) sqs
+  return (generateImage f imageSize imageSize)
+
 main :: IO ()
 main = do
-  writePng "/home/jefff/Desktop/allWhite.png" $ generatePlasma (Square origin imageSize 1 1 1 1)
-  writePng "/home/jefff/Desktop/allBlack.png" $ generatePlasma (Square origin imageSize 0 0 0 0)
-  writePng "/home/jefff/Desktop/leftToRight.png" $ generatePlasma (Square origin imageSize 1 1 0 0)
-  writePng "/home/jefff/Desktop/rightToLeft.png" $ generatePlasma (Square origin imageSize 0 0 1 1)
+  img <- generatePlasma2 (Square origin imageSize 0.9 0.25 0.75 0.2)
+  writePng "/home/jefff/Desktop/random.png" img
+
