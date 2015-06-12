bbe38638f659604388cd4f0030d2b66a93f7ef2e
Jeff Foster <jeff.foster@acm.org>
2015-06-12 08:00:35 +0100

    remove qualification spam

--- azure-event-streams/src/Main.hs
+++ azure-event-streams/src/Main.hs
@@ -3,29 +3,30 @@
 import Network.URI
 import Data.Time.Clock.POSIX
 import Data.Digest.Pure.SHA
+import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as LB
 import Data.ByteString.Base64
 
 data AccessKey = AccessKey
                  {
-                   keyName :: B.ByteString
-                 , key :: B.ByteString
+                   keyName :: ByteString
+                 , key :: ByteString
                  } deriving (Show,Eq)
 
-type Token = B.ByteString
+type Token = ByteString
 
-encodeURI :: URI -> B.ByteString
+encodeURI :: URI -> ByteString
 encodeURI x = B.pack $ uriToString id x ""
 
-sign :: B.ByteString -> B.ByteString -> B.ByteString
+sign :: ByteString -> ByteString -> ByteString
 sign key signingString = encode $ LB.toStrict $ bytestringDigest dig
   where
     strictKey = LB.fromStrict key
     strictString = LB.fromStrict signingString
     dig = hmacSha1 strictString strictString
 
-buildUri :: B.ByteString -> B.ByteString -> Integer -> B.ByteString -> B.ByteString
+buildUri :: ByteString -> ByteString -> Integer -> ByteString -> ByteString
 buildUri uri signature expiry keyName = B.concat [
         "SharedAccessSignature sr=",
         uri,
@@ -41,7 +42,7 @@ createSASToken :: URI -> AccessKey -> IO Token
 createSASToken uri accessKey = do
   expiry <- round `fmap` getPOSIXTime
   let signingString = B.concat [encodeURI uri, "\n", B.pack $ show expiry]
-      signature :: B.ByteString
+      signature :: ByteString
       signature = sign (key accessKey) signingString
   return $ buildUri (encodeURI uri) signature expiry (keyName accessKey)
 
